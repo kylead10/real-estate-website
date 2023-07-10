6133028caf8fde7fcc2c07676aecef33e181b594
kylead10 <adiokayode111@gmail.com>
2023-07-10 14:25:34 +0100

    built with react

---
+++ src/data/InfoData.js
@@ -0,0 +1,26 @@
+import ImageOne from '../images/image5.jpg';
+import ImageTwo from '../images/image3.jpg';
+
+export const InfoData = {
+  heading: 'Explore our beautiful houses',
+  paragraphOne:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, nemo cum? Commodi mollitia nulla necessitatibus velit et odio, dicta adipisci rerum delectus laudantium, ea eos!',
+  paragraphTwo:
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem neque voluptas ullam culpa eos! Delectus.',
+  buttonLabel: 'View Homes',
+  image: ImageOne,
+  reverse: false,
+  delay: 100,
+};
+
+export const InfoDataTwo = {
+  heading: 'Modern home designs',
+  paragraphOne:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, nemo cum? Commodi mollitia ci rerum delectus laudantium, ea eos!',
+  paragraphTwo:
+    'Lorem consectetur adipisicing elit. Dolorem neque voluptas ullam culpa eos! Delectus.',
+  buttonLabel: 'View Homes',
+  image: ImageTwo,
+  reverse: true,
+  delay: 300,
+};
---
+++ src/data/SliderData.js
@@ -0,0 +1,69 @@
+import ImageOne from '../images/image6.jpg';
+import ImageTwo from '../images/image4.jpg';
+import ImageThree from '../images/image1.jpg';
+import ImageFour from '../images/image2.jpg';
+import ImageFive from '../images/image5.jpg';
+import ImageSix from '../images/image3.jpg';
+import ImageSeven from '../images/image7.jpg';
+
+export const SliderData = [
+  {
+    title: 'Luxury Villa in Banana Island Lagos, Nigeria',
+    price: '#220,000,000',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageOne,
+    alt: 'House',
+  },
+  {
+    title: 'House in Oluyole Ibadan, Oyo state, Nigeria',
+    price: '#48,000,000',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageTwo,
+    alt: 'House',
+  },
+  {
+    title: 'Luxury Villa in Lekki phase1 Lagos, Nigeria',
+    price: '#90,000,000',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageThree,
+    alt: 'House',
+  },
+  {
+    title: 'House in GRA Ibadan, Oyo state, Nigeria',
+    price: '#37,000,000',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageFour,
+    alt: 'House',
+  },
+];
+
+export const SliderDataTwo = [
+  {
+    title: 'House in Elebu Ibadan, Oyo state, Nigeria',
+    price: '#44,000,000',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageFive,
+    alt: 'House',
+  },
+  {
+    title: 'House in Surulere Lagos, Nigeria',
+    price: '#56,000,000',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageSix,
+    alt: 'House',
+  },
+  {
+    title: 'House in Maryland Lagos, Nigeria',
+    price: '#41,000,000',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageSeven,
+    alt: 'House',
+  },
+];
